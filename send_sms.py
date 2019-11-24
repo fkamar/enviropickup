227e0f94ddef5490ad616a608101ed7357d20956
fkamar <37725741+fkamar@users.noreply.github.com>
2019-11-24 09:09:21 -0500

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -1,12 +1,12 @@
 import os
 from twilio .rest import Client
 
-account_sid = "AC2ef3b3f1129a5c58646cd19aee908ebf"
-auth_token = "24c223d8c58749119ef0f2110387b74c"
+account_sid = "******"
+auth_token = "******"
 client = Client(account_sid, auth_token)
 
-message = client.messages.create(to='+17057617145',
-                                 from_='+12262712087',
+message = client.messages.create(to='insert number',
+                                 from_='EnviroPickupNumber',
                                  body="Do you have SPECIAL trash")
 
 print(message)
